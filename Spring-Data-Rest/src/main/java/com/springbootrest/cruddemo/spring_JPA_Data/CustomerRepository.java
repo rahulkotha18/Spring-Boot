776f5479d4e57d39030ef3d8fccf62ec4531b8b9
Rahul Kotha <rahulhopper047@gmail.com>
2019-08-22 15:46:23 +0530

    spring data rest updated

--- Spring-Data-Rest/src/main/java/com/springbootrest/cruddemo/spring_JPA_Data/CustomerRepository.java
+++ Spring-Data-Rest/src/main/java/com/springbootrest/cruddemo/spring_JPA_Data/CustomerRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource(path = "members")
+//@RepositoryRestResource(path = "customers")
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
 }
